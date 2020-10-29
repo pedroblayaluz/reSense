6e09b7e717911b6330cd68b06e1b6842c9304854
pedroblayaluz <blaya.luz@gmail.com>
2020-10-29 11:45:26 -0300

    included an option in shpToEE to include a very specific data.frame

--- R/shptoEE.R
+++ R/shptoEE.R
@@ -3,6 +3,7 @@
 #' This function converts a Shapefile (.shp) to a
 #' Google Earth Engine Polygon (ee.Geometry.Polygon)
 #' @param shapefile Path to a .shp file
+#' @param df Generates a EE geometry from a data.frame in which the first two columns are x and y
 #' @return Google Earth Engine Polygon
 #' @export
 #' @examples
@@ -10,9 +11,13 @@
 #'
 #'
 #Shapefile to Earth Engine Geometry
-shpToEE <- function(shapefile){
+shpToEE <- function(shapefile,df=F){
+  if(df==T){
+    shapefile.polygon <- as.matrix(shapefile[,c(1,2)])
+  }else{
   shapefile.st <- sf::st_read(shapefile) #Read shapefile
   shapefile.polygon <- shapefile.st[[2]][[1]][[1]]
+  }
   shapefile.list <- split(shapefile.polygon, seq(nrow(shapefile.polygon)))
   names(shapefile.list) <- NULL
   shapefile.ee.geometry <- rgee::ee$Geometry$Polygon(shapefile.list)
